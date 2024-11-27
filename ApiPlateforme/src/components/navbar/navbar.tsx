99e4f2691f80ca5b690545b58605214aad9c817d
cocosop <corinemanuella@yahoo.fr>
2024-11-27 09:22:42 +0100

    header1

--- ApiPlateforme/src/components/navbar/navbar.tsx
+++ ApiPlateforme/src/components/navbar/navbar.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const navbar = () => {
-    return (
-        <div>
-            <h1>C'est la navbar ici</h1>
+  return (
+    <div className="contenair1">
+      <div className="contenair2">
+        <div className="nav1">
+          <img className="image1" src="../src/assets/Camer.png"></img>
+          <button className="buton" >
+            Francais
+          </button>
         </div>
-    );
+        <div>
+        </div>
+        <nav>
+          <Link to="/" className="title">
+            <img src="../src/assets/logo.png"></img>
+          </Link>
+          <ul >
+            <li>
+              <NavLink to="/about">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Secteurs</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Legislations</NavLink>
+            </li>
+            <li>
+              <NavLink to="/FAQ">FAQ</NavLink>
+            </li>
+            <button >
+              Espace privé
+            </button>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default navbar;
