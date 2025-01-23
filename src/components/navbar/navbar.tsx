4ec1df2530de62b8a1b0e8fc3a955f5506619b83
cocosop <corinemanuella@yahoo.fr>
2025-01-23 13:49:58 +0100

    conflits

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,6 +7,7 @@ import Logo from '../../assets/img/logo.png'
 import { KeyboardArrowDown } from "@mui/icons-material";
 import Grid from "@mui/system/Grid";
 import Language from "@mui/icons-material/Language";
+import React from "react";
 
 const menuData = [
   {
